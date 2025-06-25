dd6785c08f76779156bfc69397afdf23161806d2
mehedimilon20 <mehedimilon20@gmail.com>
2025-06-25 23:42:36 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "12345678"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "XXXXXXX:XXXXXXXX"  # Replace this BOT_TOKEN
+API_ID = "23830854"  # Replace this API ID with your actual API ID
+API_HASH = "2360d53e98bf778a286d8c054333d470"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "8012301824:AAE5f2-qqml-6IGf7NC8ZqPfzi6k7N393es"  # Replace this BOT_TOKEN
